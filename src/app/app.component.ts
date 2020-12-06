c64142d0304bf9a9c4f03b0690a8336388bd2180
Adamou <ibnadam2010@yahoo.fr>
2020-12-06 01:55:36 +0100

    ajout de OnInit de angular core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import {AppareilService} from './services/appareil.service';
 @Component({
   selector: 'app-root',
